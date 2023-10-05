a265e01e042b74b4319f3af18b36c1a26c5867bb
sagar <04017711721_iot@vips.edu>
2023-10-05 13:12:17 +0530

    password generator Complete

--- 05password/src/App.jsx
+++ 05password/src/App.jsx
@@ -1,8 +1,9 @@
-import { useState , useCallback, useEffect} from 'react'
+import { useState , useCallback, useEffect , useRef} from 'react'
 
 import './App.css'
 
 function App() {
+  const passRef = useRef(null)
   const [len, setLen ] = useState(8);
   const [numberAllow  , setnumberAllow ] = useState(false);
   const [charAllow  , setcharAllow] = useState(false);
@@ -17,16 +18,22 @@ function App() {
 
     for ( let i=1 ; i <= len ; i++){
       let char = Math.floor(Math.random() * str.length +1);
-      pass = str.charAt(char)
+      pass += str.charAt(char)
     }
 
     setpassword(pass);
-  } , [ len , numberAllow ,charAllow  ])
+  } , [ len , numberAllow ,charAllow , setpassword])
 
   useEffect( () => {
     passwordGenerator()
-  } , [ len , numberAllow ,charAllow  ])
+  } , [len, numberAllow, charAllow, passwordGenerator])
 
+
+  const CopytoClib =() =>{
+    passRef.current?.select();
+    passRef.current?.setSelectionRange(0, 500);
+    window.navigator.clipboard.writeText(password)
+  }
   return (
     <>
       <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
@@ -38,9 +45,10 @@ function App() {
             className="outline-none w-full py-1 px-3"
             placeholder="Password"
             readOnly
+            ref={passRef }
         />
         <button
-
+        onClick={CopytoClib}
         className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
         >copy</button>
         
@@ -49,8 +57,8 @@ function App() {
       <div className='flex items-center gap-x-1'>
         <input 
         type="range"
-        min={6}
-        max={100}
+        min={8}
+        max={25}
         value={length}
           className='cursor-pointer'
           onChange={(e) => {setLen(e.target.value)}}
